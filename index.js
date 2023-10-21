d204804e175c2b86a70f832e836511a93b1af17e
Md Miftahul Islam <98267581+itzmiftah@users.noreply.github.com>
2023-10-21 18:39:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// const arr = [
+//     [1, 2, 3, 4],
+//     [1, 2, 3, 4],
+//     [1, 2, 3, 4],
+//     [1, 2, 3, 4],
+//   ];
+//   const n = arr.length;
+//   // console.log(arr);
+//   for (let i = 0; i < n; i++) { // 
+//     let ans = "";
+//     if(i%2==0){ 
+//     for (let j = 0; j < n; j++) {
+//         // if(i==j)// 3
+//         // if(i+j==n-1)
+         
+//       ans += arr[i][j] + " ";  // 00 01 02 03  1 2 3 4
+//                                // 10 11 12 13   
+//                                // 20 21 22 23  
+//                                // 30 31 32 33  
+//     }
+// }
+// else {
+//     for (let j =0; j<n; j++)
+//     ans += arr[i][j] + " "; 
+// }
+//     console.log(ans);
+//   }
+// // ------------------------------------------slice
+// // // let text = "AppleApplebb";
+// // // let part = text.slice(0,5);
+// // // console.log(part)
+// // --------------------------------------------splice
+// // let text =["miftah","saheb","zara"];
+// // let part = text.splice(1,1,"hh");
+// //  console.log(text)
+// // -----------------------------------------map
+// // let arr = [1,3,5];
+
+// // let a = arr.map((value)=>{ 
+// //   return value*value;
+// // })
+// // console.log(a)
+// // ------------------------------------------filter
+
+// // let arr = [1,3,5,20,40];
+// // let a = arr.filter((a2)=>{
+// //    return a2<10
+// // })
+
+// // console.log(a)
+
+// // // -------------------------------reduce
+// // let arr = [1,3,5,20,40];
+ 
+// //   const func = (h1,h2) =>{
+// //      return h1+h2;
+// //   }
+// //   let a= arr.reduce(func)
+// // console.log(a)
+// // ------------------------------ literals
+//  const a = "miftah";
+//  const b = "hello" +" "+ a;
+//  console.log(b);
